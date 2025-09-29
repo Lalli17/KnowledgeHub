935ff42d39410b854231eda2f7961f9854f29351
aanchalm010 <aanchalmaurya010@gmail.com>
2025-09-29 22:44:48 +0530

    Revert "ReviewsUpdated"
    This reverts commit 7d1836e8ea57cb80b0301161b4907bbcf64b7d12.

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   // Use the URL from your backend's launchSettings.json
-  apiBaseUrl: 'https://localhost:7298/api',
+  apiBaseUrl: 'https://knowledgehubdotnet.azurewebsites.net/api',
  
 };
